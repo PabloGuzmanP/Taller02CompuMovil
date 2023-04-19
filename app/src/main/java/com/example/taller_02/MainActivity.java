3524a77ae42e13ed59d694ac55ea90c8bad4d5a2
juanpagu2101 <guzmanpjpablo@gmail.com>
2023-04-18 23:27:54 -0500

    Parte de la camara

--- app/src/main/java/com/example/taller_02/MainActivity.java
+++ app/src/main/java/com/example/taller_02/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.taller_02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.taller_02.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.botoncamara);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openNewActivity();
+            }
+        });
+
+    }
+    public void openNewActivity(){
+        Intent intent = new Intent(this, Camara.class);
+        startActivity(intent);
     }
 }
